4d266060512ac6a636b80fa7c069d16fb1abcbb9
grgpurnima27 <grgpurnima27@gmail.com>
2026-05-05 16:07:54 +0545

    done

---
+++ books/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    name= models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title= models.CharField(max_length=52)
+    author=models.CharField(max_length=50)
+    published_date= models.DateField()
+    isbn= models.CharField(max_length=13, unique=True)
+
+    category= models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name="books"
+    )
+
+    def __str__(self):
+        return self.title
---
+++ books/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Book, Category
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model= Category
+        fields="__all__"
+class BookSerializer(serializers.ModelSerializer):
+    category_name= serializers.ReadOnlyField(source="category.name")
+    class Meta:
+        model= Book
+        fields= [
+            'id','title','author','published_date','category','category_name'
+        ]
