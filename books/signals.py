f21c7e6f391eb70398c656299bfd97736051ba76
grgpurnima27 <grgpurnima27@gmail.com>
2026-05-07 12:39:18 +0545

    modified : understood

--- books/signals.py
+++ books/signals.py
@@ -5,3 +5,4 @@ from .utils import send_reset_email
 @receiver(reset_password_token_created)
 def password_reset_token_created(sender, instance, reset_password_token, *args, **kwargs):
     send_reset_email(reset_password_token.user.email, reset_password_token.key)
+    
--- books/urls.py
+++ books/urls.py
@@ -1,15 +1,3 @@
-# from django.urls import path, include
-# from rest_framework.routers import DefaultRouter
-# from .views import BookViewSet, CategoryViewSet, 
-
-# router= DefaultRouter()
-# router.register(r'books',BookViewSet)
-# router.register(r'categories',CategoryViewSet)
-
-# urlpatterns = [
-#     path('api/', include(router.urls)),
-#     path('reset-password/<str:token>/', views.reset_password_page),
-# ]
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import BookViewSet, CategoryViewSet
