42936b148ef5ae20251cd001cf5ffec8b058b00e
grgpurnima27 <grgpurnima27@gmail.com>
2026-05-08 16:29:07 +0545

    add static_root

--- config/settings.py
+++ config/settings.py
@@ -311,4 +311,5 @@ LOGIN_URL = '/admin/login/'
 
 # Static files
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # ← add this line
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
