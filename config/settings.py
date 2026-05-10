47ea26fab8258c916bdc886b8d76542419342efd
grgpurnima27 <grgpurnima27@gmail.com>
2026-05-10 14:36:52 +0545

    fix mail port 465 ssl

--- config/settings.py
+++ config/settings.py
@@ -304,8 +304,9 @@ PASSWORD_RESET_CONFIRM_URL = 'reset-password/{token}/'
 # =========================
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True 
+EMAIL_USE_TLS = False     
 
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
