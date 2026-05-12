7f6191553c6d3a49e624c62a6e2e59335fd302fb
grgpurnima27 <grgpurnima27@gmail.com>
2026-05-12 15:31:53 +0545

    fix:updated files

--- books/utils.py
+++ books/utils.py
@@ -1,12 +1,22 @@
 from django.core.mail import EmailMessage
 from django.conf import settings
 
-def send_reset_email(to_email, subject, html_content):
+def send_reset_email(to_email, token, html_content=None):
+
+    reset_link = f"{settings.BASE_URL}/reset-password/{token}/"
+
+    if html_content is None:
+        html_content = f"""
+        <h2>Password Reset</h2>
+        <p>Click below to reset password:</p>
+        <a href="{reset_link}">{reset_link}</a>
+        """
+
     email = EmailMessage(
-        subject=subject,
+        subject="Reset Your Password",
         body=html_content,
         from_email=settings.DEFAULT_FROM_EMAIL,
         to=[to_email],
     )
     email.content_subtype = "html"
-    email.send()
+    email.send(fail_silently=False)
