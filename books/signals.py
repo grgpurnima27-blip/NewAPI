51f37804fbce45d14e86243087b5b972c36b07c5
grgpurnima27 <grgpurnima27@gmail.com>
2026-05-12 16:35:13 +0545

    fix:updated signles.py

--- books/signals.py
+++ books/signals.py
@@ -4,7 +4,18 @@ from .utils import send_reset_email
 
 @receiver(reset_password_token_created)
 def password_reset_token_created(sender, instance, reset_password_token, *args, **kwargs):
+
+    email = reset_password_token.user.email
+    token = reset_password_token.key
+
+    reset_link = f"https://newapi-jgbv.onrender.com/reset-password/{token}/"
+
     send_reset_email(
-        reset_password_token.user.email,
-        reset_password_token.key
+        to_email=email,
+        subject="Reset Your Password",
+        html_content=f"""
+            <h2>Password Reset</h2>
+            <p>Click below to reset your password:</p>
+            <a href="{reset_link}">{reset_link}</a>
+        """
     )
--- books/utils.py
+++ books/utils.py
@@ -1,17 +1,12 @@
 from django.core.mail import EmailMessage
 from django.conf import settings
 
-def send_reset_email(to_email, token):
-    reset_link = f"{settings.BASE_URL}/reset-password/{token}/"
-
+def send_reset_email(to_email, subject, html_content):
     email = EmailMessage(
-        subject="Reset Password",
-        body=f"Click here: {reset_link}",
+        subject=subject,
+        body=html_content,
         from_email=settings.DEFAULT_FROM_EMAIL,
         to=[to_email],
     )
-
-    try:
-        email.send(fail_silently=False)
-    except Exception as e:
-        print("EMAIL ERROR:", e)
+    email.content_subtype = "html"
+    email.send(fail_silently=False)
