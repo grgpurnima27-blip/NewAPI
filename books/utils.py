825ef46c878b33451185c87aef23deae93835f63
grgpurnima27 <grgpurnima27@gmail.com>
2026-05-12 15:06:34 +0545

    fix:utils.py

--- books/utils.py
+++ books/utils.py
@@ -1,47 +1,12 @@
-from mailjet_rest import Client
+from django.core.mail import EmailMessage
 from django.conf import settings
 
-
-def send_reset_email(email, token):
-
-    reset_link = f"{settings.BASE_URL}/reset-password/{token}/"
-
-    mailjet = Client(
-        auth=(
-            settings.MAILJET_API_KEY,
-            settings.MAILJET_API_SECRET
-        ),
-        version='v3.1'
+def send_reset_email(to_email, subject, html_content):
+    email = EmailMessage(
+        subject=subject,
+        body=html_content,
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        to=[to_email],
     )
-
-    data = {
-        'Messages': [
-            {
-                "From": {
-                    "Email": settings.MAILJET_SENDER_EMAIL,
-                    "Name": settings.MAILJET_SENDER_NAME
-                },
-                "To": [
-                    {
-                        "Email": email,
-                    }
-                ],
-                "Subject": "Password Reset",
-                "HTMLPart": f"""
-                    <p>Click below to reset your password:</p>
-                    <p>
-                        <a href="{reset_link}">
-                            Reset Password
-                        </a>
-                    </p>
-                """
-            }
-        ]
-    }
-
-    result = mailjet.send.create(data=data)
-
-    print(result.status_code)
-    print(result.json())
-
-    return result.json()
+    email.content_subtype = "html"
+    email.send()
--- config/settings.py
+++ config/settings.py
@@ -444,9 +444,10 @@ ROOT_URLCONF = 'config.urls'
 # ================= DATABASE =================
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+    'default': dj_database_url.parse(
+        os.environ.get("DATABASE_URL"),
         conn_max_age=600,
+        ssl_require=True
     )
 }
 
